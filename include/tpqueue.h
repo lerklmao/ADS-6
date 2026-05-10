4747faa6ac2bacfa1b98df718036008e56cfedcd
lerklmao <owins.12KVE@gmail.com>
2026-05-10 22:42:27 +0300

    Королева Валерия ИСТ-4-1

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,50 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+ private:
+  struct Node {
+    T data;
+    Node* next;
+    explicit Node(const T& value) : data(value), next(nullptr) {}
+  };
+  Node* head;
+
+ public:
+  TPQueue() : head(nullptr) {}
+
+  ~TPQueue() {
+    while (head) {
+      Node* temp = head;
+      head = head->next;
+      delete temp;
+    }
+  }
+
+  void push(const T& value) {
+    Node* newNode = new Node(value);
+    if (!head || value.prior > head->data.prior) {
+      newNode->next = head;
+      head = newNode;
+    } else {
+      Node* current = head;
+      while (current->next && current->next->data.prior >= value.prior) {
+        current = current->next;
+      }
+      newNode->next = current->next;
+      current->next = newNode;
+    }
+  }
+
+  T pop() {
+    if (!head) {
+      throw "Queue is empty";
+    }
+    Node* temp = head;
+    T result = head->data;
+    head = head->next;
+    delete temp;
+    return result;
+  }
 };
 
 struct SYM {
